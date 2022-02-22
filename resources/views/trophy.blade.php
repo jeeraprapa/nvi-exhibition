37b6f6ec6ab9e4b788cc7313005a499bb31f65bf
jeeraprapa <jeeraprapa.h@gmail.com>
2022-02-23 00:42:52 +0700

    add css hall of fame

--- resources/views/trophy.blade.php
+++ resources/views/trophy.blade.php
@@ -1,21 +1,19 @@
 @extends('layouts.base')
 
 @section('content')
-    <div id="hall" class="w-100 position-absolute">
-        <h1 class="position-absolute text-center p-4 mt-4 w-100 fw-bold" style="
-            -webkit-text-stroke: 2px #FFFFFF;
-            -webkit-text-fill-color: #243876;
-            text-shadow: 0px 0px 15px #FFFFFF;
-            top: 30px;color: #243876;
-            font-size: 40px;
-        ">
+    <div id="hall" class="position-relative p-4">
+        <h1 class="w-100 fw-bold">
             รางวัลผู้ทำคุณประโยชน์ในการพัฒนางานวัคซีน ประจำปี พ.ศ. 2564
         </h1>
-        <img src="{{asset('images/hall_bg.png')}}" alt="" class="img-fluid">
-    </div>
-
-    <div id="trophy" class="w-100 position-relative" style="margin-top: -30px">
-        <img src="{{asset('images/award1.png')}}" alt="" width="w-50">
-        <img src="{{asset('images/award2.png')}}" alt="" width="w-50">
+        <div id="trophy" class="w-100 position-relative d-flex justify-content-lg-end">
+            <div class="row">
+                <div class="col-12 col-md-6 p-4">
+                    <img src="{{asset('images/award1.png')}}" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 p-4">
+                    <img src="{{asset('images/award2.png')}}" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
