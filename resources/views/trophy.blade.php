512db5d550cba0b333ca97011a6627868746075b
jeeraprapa <jeeraprapa.h@gmail.com>
2022-03-04 00:28:55 +0700

    change img trophy

--- resources/views/trophy.blade.php
+++ resources/views/trophy.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.base')
 
 @section('content')
-    <div id="hall" class="position-relative p-4">
+    <div id="hall" class="position-relative px-4">
         <h1 class="w-100 fw-bold">
             รางวัลผู้ทำคุณประโยชน์ในการพัฒนางานวัคซีน ประจำปี พ.ศ. 2564
         </h1>
-        <div id="trophy" class="w-100 position-relative d-flex justify-content-lg-end">
+        <div id="trophy" class="w-100 position-relative d-flex justify-content-lg-end mt-4">
             <div class="row">
                 <div class="col-12 col-md-6 p-4">
                     <img src="{{asset('images/award1.png')}}" alt="" class="img-fluid">
